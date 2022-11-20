5c053867ad85c0edb71501fc290373284f6badac
zhouzhouce <zhouzhouce@gmail.com>
2022-11-20 01:23:25 -0500

    update seed type in Simulator

--- src/Simulator.java
+++ src/Simulator.java
@@ -2,11 +2,12 @@ import java.util.Random;
 
 public class Simulator {
 
+    private static final long SEED = 1;
+
     public static void main(String argv[]) {
         Airport airport = Airport.getInstance();
         // Set up seed value for each simulation
-        int seed = 1;
-        airport.simulate(seed);
+        airport.simulate(SEED);
         airport.generateReport();
     }
 }
